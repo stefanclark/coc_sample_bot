f97091ed838e0ac98b08582ba8888e9b9dfc881d
Patrick <wpmjones@gmail.com>
2021-02-18 09:40:53 -0800

    just tidying up some things

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 # Import other libraries to be used in your code
 import coc
-import traceback
 import creds
-from database.database import BotDatabase
+import traceback
 
+from database.database import BotDatabase
 from discord.ext import commands
 
 description = "This is where you provide a concise description of your bot. Not sure if this is ever visible anywhere."
@@ -12,7 +12,6 @@ description = "This is where you provide a concise description of your bot. Not 
 prefix = "!"
 
 # Here you make the connection to the COC API using the coc.py library
-
 coc_client = coc.login(creds.coc_dev_email, creds.coc_dev_password, client=coc.EventsClient, key_names="war_home")
 
 # These are the cogs that you are using in your bot
@@ -35,7 +34,7 @@ class MyBot(commands.Bot):
                          description=description,
                          case_insensitive=True)
         self.coc = coc_client
-        # This instanciates the database class
+        # This instantiates the database class
         self.dbconn = BotDatabase(SQLITE_FILE)
 
         # Load all extensions (see the cogs folder)
